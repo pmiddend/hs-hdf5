15d3bcb37d6391510959167d58ca0d8736b215a4
Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>
2024-08-02 09:48:36 +0200

    Added a script to publish the doc.
    I do not know if hackage produce the right documentation  so provide a
    script which simplify the publish process.

---
+++ publish-doc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+dir=$(mktemp -d dist-docs.XXXXXX)
+trap 'rm -r "$dir"' EXIT
+
+# assumes cabal 2.4 or later
+cabal v2-haddock --builddir="$dir" --haddock-for-hackage --enable-doc
+
+# candidate
+cabal upload -d $dir/*-docs.tar.gz
